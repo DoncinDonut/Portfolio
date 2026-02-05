4c088428ad4e2ab6e67341c510c5d4b06dc7aa6a
Cian Donnelly <233502017+DoncinDonut@users.noreply.github.com>
2026-02-05 22:04:41 +0000

    .

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -95,7 +95,7 @@ export default function DashboardPage() {
 
   <IconButton
     component="a"
-    href="/CV.pdf"
+    href="public/CV.pdf"
     target="_blank"
     rel="noopener noreferrer"
     sx={{ color: 'white' }}
