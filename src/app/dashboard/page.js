99e0f29671e312eff3261917d92dec39b4104cca
Cian Donnelly <233502017+DoncinDonut@users.noreply.github.com>
2026-02-05 21:05:47 +0000

    .

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -95,7 +95,7 @@ export default function DashboardPage() {
 
   <IconButton
     component="a"
-    href="/Cian_Donnelly_CV.pdf"
+    href="/public/CV.pdf"
     target="_blank"
     rel="noopener noreferrer"
     sx={{ color: 'white' }}
